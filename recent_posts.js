808a9474a1c93dfc6645ef2839a778f893eb5d04
Pavlo Sirous <pashka4281@gmail.com>
2015-10-11 23:29:20 +0300

    Site updated at 2015-10-11 20:29:20 UTC

---
+++ recent_posts.js
@@ -0,0 +1,63 @@
+var posts = [];
+
+
+
+
+
+
+
+
+
+  
+
+
+ posts.push({ link: "/blog/2015/08/03/mysql-gem-float-rounding/", title: "mysql gem float rounding", date: "Aug 3<span>rd</span>, 2015" }); 
+
+
+
+
+
+
+
+
+  
+
+
+ posts.push({ link: "/blog/2015/07/29/night-work-plus-eyes-safety-equals-f-dot-lux/", title: "Night work + eyes safety = f.lux", date: "Jul 29<span>th</span>, 2015" }); 
+
+
+
+
+
+
+
+
+  
+
+
+ posts.push({ link: "/blog/2015/05/10/who-wants-to-pair-together-with-me/", title: "Who wants to pair together with me?", date: "May 10<span>th</span>, 2015" }); 
+
+
+
+
+
+
+
+
+  
+
+
+ posts.push({ link: "/blog/2015/05/06/ssh-port-forwarding-how-to/", title: "SSH port forwarding: how to?", date: "May 6<span>th</span>, 2015" }); 
+
+
+
+
+
+
+
+
+  
+
+
+ posts.push({ link: "/blog/2015/03/06/rails-e-tags-explained/", title: "Rails E-tags explained", date: "Mar 6<span>th</span>, 2015" }); 
+recentPostsCallback(posts);
