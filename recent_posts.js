33d79784f7907df3e0308fc81e6b970b37d35776
Pavlo Sirous <pashka4281@gmail.com>
2015-10-17 21:30:47 +0300

    Site updated at 2015-10-17 18:30:47 UTC

--- recent_posts.js
+++ recent_posts.js
@@ -11,19 +11,9 @@ var posts = [];
   
 
 
- posts.push({ link: "/blog/2015/08/03/mysql-gem-float-rounding/", title: "mysql gem float rounding", date: "Aug 3<span>rd</span>, 2015" }); 
 
+  posts.push({ link: "/blog/2015/10/17/das-keyboard-4c-profesional-review/", title: "A post about mechanical keyboards | Das Keyboard 4c professional review", date: "Oct 17<span>th</span>, 2015" });
 
-
-
-
-
-
-
-  
-
-
- posts.push({ link: "/blog/2015/07/29/night-work-plus-eyes-safety-equals-f-dot-lux/", title: "Night work + eyes safety = f.lux", date: "Jul 29<span>th</span>, 2015" }); 
 
 
 
@@ -35,19 +25,9 @@ var posts = [];
   
 
 
- posts.push({ link: "/blog/2015/05/10/who-wants-to-pair-together-with-me/", title: "Who wants to pair together with me?", date: "May 10<span>th</span>, 2015" }); 
 
+  posts.push({ link: "/blog/2015/08/03/mysql-gem-float-rounding/", title: "mysql gem float rounding", date: "Aug 3<span>rd</span>, 2015" });
 
-
-
-
-
-
-
-  
-
-
- posts.push({ link: "/blog/2015/05/06/ssh-port-forwarding-how-to/", title: "SSH port forwarding: how to?", date: "May 6<span>th</span>, 2015" }); 
 
 
 
@@ -59,5 +39,35 @@ var posts = [];
   
 
 
- posts.push({ link: "/blog/2015/03/06/rails-e-tags-explained/", title: "Rails E-tags explained", date: "Mar 6<span>th</span>, 2015" }); 
+
+  posts.push({ link: "/blog/2015/07/29/night-work-plus-eyes-safety-equals-f-dot-lux/", title: "Night work + eyes safety = f.lux", date: "Jul 29<span>th</span>, 2015" });
+
+
+
+
+
+
+
+
+
+  
+
+
+
+  posts.push({ link: "/blog/2015/05/10/who-wants-to-pair-together-with-me/", title: "Who wants to pair together with me?", date: "May 10<span>th</span>, 2015" });
+
+
+
+
+
+
+
+
+
+  
+
+
+
+  posts.push({ link: "/blog/2015/05/06/ssh-port-forwarding-how-to/", title: "SSH port forwarding: how to?", date: "May 6<span>th</span>, 2015" });
+
 recentPostsCallback(posts);
